1f5364ddf3a39ce391c8c588bd3c8c6df15e3fe9
Артём Крыжевич <artem.kryzhevich.a@yandex.ru>
2021-10-13 13:38:20 +0300

    Initial commit

---
+++ Laba1/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Laba1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ЗагрузитьТекстДляШифрованияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ЗагрузитьТекстДляШифрованияToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ЗагрузитьТекстДляШифрованияToolStripMenuItem
+            // 
+            this.ЗагрузитьТекстДляШифрованияToolStripMenuItem.Name = "ЗагрузитьТекстДляШифрованияToolStripMenuItem";
+            this.ЗагрузитьТекстДляШифрованияToolStripMenuItem.Size = new System.Drawing.Size(202, 20);
+            this.ЗагрузитьТекстДляШифрованияToolStripMenuItem.Text = "Загрузить текст для шифрования";
+            this.ЗагрузитьТекстДляШифрованияToolStripMenuItem.Click += new System.EventHandler(this.ЗагрузитьТекстДляШифрованияToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(12, 256);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 248);
+            this.panel1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 28);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(775, 114);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 148);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(450, 102);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Размер решетки Кардано";
+            this.groupBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(301, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(183, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(139, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(20, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(191, 510);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Зашифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(467, 510);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 45);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Дешифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(485, 148);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(303, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Случайные ключи";
+            this.groupBox2.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(49, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(211, 31);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Сгенерировать случайные ключи";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 559);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Решетка Кардано";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ЗагрузитьТекстДляШифрованияToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Laba1/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Laba1
+{
+    public partial class Form1 : Form
+    {
+        private static int count_columns =0;
+        private static int count_rows = 0;
+        private static int size = 0;
+        private static int location_x = 10;
+        private static int location_y = 10;
+        private static int count_square = 0;
+        private static bool[,] bool_mass;
+        private static string russian_alphavite = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ `~!@#$%^&*()_+-=/\\|.,<>?:;\"'[]{}№"; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ЗагрузитьТекстДляШифрованияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "txt files(*.txt)| *.txt";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    string text_for_encrypt = File.ReadAllText(ofd.FileName, Encoding.Default);
+                    bool success = true;
+                    for (int i = 0; i < text_for_encrypt.Length; i++)
+                    {
+                        if (!russian_alphavite.Contains(text_for_encrypt[i]))
+                        {
+                            MessageBox.Show($"{text_for_encrypt[i]}");
+                            success = false;
+                            break;
+                        }
+                        
+                    }
+                    if (success)
+                    {
+                        count_square = 0;
+                        for (int row = 0; row < count_rows; row++)
+                        {
+                            for (int column = 0; column < count_columns; column++)
+                            {
+                                bool_mass[row, column] = false;
+                            }
+                        }
+                        richTextBox1.Text = text_for_encrypt;
+                        richTextBox1.Visible = true;
+                        groupBox1.Visible = true;
+                        groupBox2.Visible = false;
+                        button2.Visible = false;
+                        button3.Visible = false;
+                        textBox1.Text = String.Empty;
+                        textBox2.Text = String.Empty;
+                        panel1.Controls.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Программа работает только с русскими символами и знаками пунктуации. Повторите загрузку файла!");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != String.Empty && textBox2.Text != String.Empty)
+                {
+                    count_square = 0;
+                   
+                    count_columns = Convert.ToInt32(textBox1.Text);
+                    count_rows = Convert.ToInt32(textBox2.Text);
+                    size = count_columns * count_rows;
+                    if (size > richTextBox1.Text.Length)
+                    {
+                        location_x = 10;
+                        location_y = 10;
+                        panel1.Controls.Clear();
+                        bool_mass = new bool[count_rows,count_columns];
+                        for (int row = 0; row < count_rows; row++)
+                        {
+                            for (int column = 0; column < count_columns; column++)
+                            {
+                                bool_mass[row, column] = false;
+                            }
+                        }
+                        for (int row = 0; row < count_rows; row++)
+                        {
+                            for (int column = 0; column < count_columns; column++)
+                            {
+                                Label picture = new Label();
+                                picture.BorderStyle = BorderStyle.FixedSingle;
+                                picture.Size = new Size(50, 50);
+                                picture.Location = new Point(location_x, location_y);
+                                picture.Name = $"{row}:{column}";
+                                picture.Click += Picture_Click;
+                                picture.Text = "-";
+                                picture.Font = new Font("Times New Roman", 14f);
+                                picture.TextAlign = ContentAlignment.MiddleCenter;
+                                location_x += picture.Size.Width + 10;
+                                panel1.Controls.Add(picture);
+                            }
+                            location_x = 10;
+                            location_y += 50+10;
+                        }
+                        groupBox2.Visible = true;
+                        button2.Visible = false;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Размер решетки не может быть меньше, чем количество символов в шифруемом тексте!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Введите размер решетки Кардано");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void Picture_Click(object sender, EventArgs e)
+        {
+            Label picture = (Label)sender;
+            bool flag = true;
+            int index_of_separator = picture.Name.IndexOf(':');
+            int x_mass = Convert.ToInt32(picture.Name.Substring(0, index_of_separator));
+            int y_mass = Convert.ToInt32(picture.Name.Substring(index_of_separator + 1, picture.Name.Length - index_of_separator - 1));
+            if (picture.Text == "+")
+            {
+                picture.Text = "-";
+                flag = false;
+                count_square--;
+                bool_mass[x_mass, y_mass] = false;
+                button2.Visible = false;
+            }
+            if (flag)
+            {
+                if (picture.Text == "-")
+                {
+                    if (count_square != richTextBox1.Text.Length)
+                    {
+                        picture.Text = "+";
+                        count_square++;
+                        bool_mass[x_mass, y_mass] = true;
+                        if (count_square != richTextBox1.Text.Length)
+                        {
+                            button2.Visible = false;
+                        }
+                        else
+                        {
+                            button2.Visible = true;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Количество выбранных квадратов равно длине текста!");
+
+                    }
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            int cursorPosition = textBox1.SelectionStart;
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int cursorPosition = textBox2.SelectionStart;
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index_symbol = 0;
+            try
+            {
+                Random rand = new Random();
+                for (int i = 0; i < panel1.Controls.Count; i++)
+                {
+                    Label label = (Label)panel1.Controls[i];
+                    int index_of_separator = label.Name.IndexOf(':');
+                    int x_mass = Convert.ToInt32(label.Name.Substring(0, index_of_separator));
+                    int y_mass = Convert.ToInt32(label.Name.Substring(index_of_separator + 1, label.Name.Length - index_of_separator - 1));
+                    if (bool_mass[x_mass,y_mass] == true && index_symbol < richTextBox1.Text.Length)
+                    {
+                        label.Text = richTextBox1.Text[index_symbol].ToString();
+                        index_symbol++;
+                    }
+                    if(bool_mass[x_mass, y_mass] == false)
+                    {
+                        label.Text = ((char)rand.Next(0x0410, 0x44F)).ToString();
+                    }
+                }
+                richTextBox1.Text = String.Empty;
+                richTextBox1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = true;
+                groupBox1.Visible = false;
+                groupBox2.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < panel1.Controls.Count; i++)
+            {
+                Label label = (Label)panel1.Controls[i];
+                int index_of_separator = label.Name.IndexOf(':');
+                int x_mass = Convert.ToInt32(label.Name.Substring(0, index_of_separator));
+                int y_mass = Convert.ToInt32(label.Name.Substring(index_of_separator + 1, label.Name.Length - index_of_separator - 1));
+                if (bool_mass[x_mass, y_mass])
+                {
+                    label.BackColor = Color.Aqua;
+                }
+                else
+                {
+                    label.Text = String.Empty;
+                }
+            }
+            button3.Visible = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                count_square = 0;
+                int count = 0;
+                for (int row = 0; row < count_rows; row++)
+                {
+                    for (int column = 0; column < count_columns; column++)
+                    {
+                        bool_mass[row, column] = false;
+                    }
+                }
+                Random rand = new Random();
+                while (count != richTextBox1.Text.Length)
+                {
+                    int index_column = rand.Next(0, count_columns);
+                    int index_row = rand.Next(0, count_rows);
+                    if (!bool_mass[index_row, index_column])
+                    {
+                        bool_mass[index_row, index_column] = true;
+                        count++;
+                    }
+                }
+                int index = 0;
+                for (int row = 0; row < count_rows; row++)
+                {
+                    for (int column = 0; column < count_columns; column++)
+                    {
+                        if (bool_mass[row, column])
+                        {
+                            panel1.Controls[index].Text = "+";
+                            count_square++;
+                        }
+                        else
+                        {
+                            panel1.Controls[index].Text = "-";
+                        }
+                        index++;
+                    }
+                }
+                if (count_square == richTextBox1.Text.Length)
+                {
+                    button2.Visible = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
